55e89063b3dffc10695f5b2eb5fd2c6fc8cd949d
Александр Крошко <akroshko1995@gmail.com>
2023-07-14 01:16:35 +0300

    logger added

---
+++ logger.py
@@ -0,0 +1,11 @@
+import sys
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(stream=sys.stdout)
+handler.setFormatter(
+    logging.Formatter(fmt="[%(asctime)s: %(levelname)s] %(message)s")
+)
+logger.addHandler(handler)
